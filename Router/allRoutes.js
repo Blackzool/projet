637e618aad1834e197375ff8f09dc146dc5ccbab
Quentin <quentindev02@gmail.com>
2024-07-01 15:06:18 +0200

    page 404 + connexion et setup pages sites + correctifs

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,13 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/habitats", "Les Habitats", "/pages/habitats.html"),
+    new Route("/restaurations", "Restaurations", "/pages/restaurations.html"),
+    new Route("/train", "L'ArcaTrain", "/pages/train.html"),
+    new Route("/contact", "Nous Contactez", "/pages/contact.html"),
+    new Route("/signin", "Connexion", "/pages/signin.html"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Arcadia Zoo";
